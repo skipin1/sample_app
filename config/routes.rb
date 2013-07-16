5fac5ca571b0431a59046fc1bb0c539b7e7af242
Vladimir Skipin <skipin_v@mail.ru>
2013-07-16 16:17:24 +0600

    Part of g5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "static_page/home"
-  get "static_page/help"
-  get "static_page/about"
-  get "static_page/contact"
+  # get "static_page/home"
+  # get "static_page/help"
+  # get "static_page/about"
+  # get "static_page/contact"
+  match 'help', to: 'static_page#help'
+  match 'about', to: 'static_page#about'
+  match 'contact', to: 'static_page#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,7 +56,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_page#home'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,12 +9,12 @@ describe "Static pages:" do
   describe "Главная" do
 
     it "должна содержать тэг h1 'Главная'" do
-      visit '/static_page/home'
+      visit root_path
       page.should have_selector('h1', text: "Главная")
     end
 
 		it "должна содержать тэг title 'Главная'" do
-			visit '/static_page/home'
+			visit root_path
 			page.should have_selector('title', text: "#{base_title} | Главная")
 		end
 	end
@@ -22,12 +22,12 @@ describe "Static pages:" do
   describe "Страница Help" do
 
   	it "должна содержать h1 тэг 'Помощь'" do
-  		visit '/static_page/help'
+  		visit help_path
   		page.should have_selector('h1', text: "Помощь")
   	end
 
   	it "должна содержать тэг title 'Помощь'" do
-  		visit '/static_page/help'
+  		visit help_path
   		page.should have_selector('title', text: "#{base_title} | Помощь")
   	end
   end
@@ -35,12 +35,12 @@ describe "Static pages:" do
   describe "Страница О нас" do
 
   	it "должна содержать h1 тэг 'О нас'" do
-  		visit '/static_page/about'
+  		visit about_path
   		page.should have_selector('h1', text: "О нас")
   	end
 
   	it "должна содержать тэг title 'О нас'" do
-  		visit '/static_page/about'
+  		visit about_path
   		page.should have_selector('title', text: "#{base_title} | О нас")
   	end
 	end
@@ -48,17 +48,17 @@ describe "Static pages:" do
 	describe "Страница Контакты" do
 
   	it "должна содержать h1 тэг 'Контакты'" do
-  		visit '/static_page/contact'
+  		visit contact_path
   		page.should have_selector('h1', text: "Контакты")
   	end
 
   	it "должна содержать основной заголовок сайта" do
-      visit '/static_page/contact'
+      visit contact_path
       page.should have_selector('title', text: "#{base_title}")
     end
 
   	it "должна содержать тэг title 'Контакты'" do
-  		visit '/static_page/contact'
+  		visit contact_path
   		page.should have_selector('title', text: "#{base_title} | Контакты")
   	end
 	end
