b19b0037c48690d6ac78c3c36a1178cfe5f7e5f5
Vladimir Skipin <skipin_v@mail.ru>
2013-07-19 08:56:57 +0600

    some fixes & tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,35 +4,69 @@ require 'spec_helper'
 
 describe "Static pages:" do
 
-	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   subject {page}
+	#let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+  
   describe "Главная" do
     before { visit root_path }
+    let(:heading)     { 'Главная' }
+    let(:page_title)   { 'Главная' }
 
-    it { should have_selector('h1', text: "Главная") }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: 'Home' }
 
-		it { should have_selector('title', text: full_title('Главная')) }
 	end
 
   describe "Страница Помощь" do
     before { visit help_path }
+    let(:heading)     { 'Помощь' }
+    let(:page_title)   { 'Помощь' }
 
-  	it { should have_selector('h1', text: "Помощь") }
-  	it { should have_selector('title', text: full_title('Помощь')) }
+  	it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: 'Help' }
   end
 
   describe "Страница О нас" do
     before { visit about_path }
+    let(:heading)     { 'О нас' }
+    let(:page_title)   { 'О нас' }
 
-  	it { should have_selector('h1', text: "О нас") }
-  	it { should have_selector('title', text: full_title('О нас')) }
+
+  	it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: 'About' }
 	end
 
 	describe "Страница Контакты" do
     before { visit contact_path }
+    let(:heading)     { 'Контакты' }
+    let(:page_title)   { 'Контакты' }
 
-  	it { should have_selector('h1', text: "Контакты") }
-   	it { should have_selector('title', text: full_title('Контакты')) }
+  	it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: 'Contact' }
 	end
+
+  it "правильные ссылки в шаблонах" do
+    visit root_path
+
+    click_link "About"
+    page.should have_selector 'title', text: full_title('О нас')
+
+    click_link "Помощь"
+    page.should have_selector 'h1', text: 'Помощь'
+    
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Контакты')
+
+    click_link "Главная"
+    click_link "Sign up now!"
+    page.should have_selector 'h1', text: 'Регистрация'
+
+    # click_link "sample app"
+    # page.should # fill in
+  end
 end
